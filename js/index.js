f2a66ff71de4dfad2440579f034e48dfc593def7
Amit Giri <giriamit987@gmail.com>
2021-12-05 14:47:44 +0530

    all good

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.getElementById("myBtn").addEventListener("mouseover", function() {
+    document.getElementById("demo").innerHTML = "Please Fill This Form & Sign in";
+    this.style.backgroundColor = "black";
+  });
+
+    document.getElementById("myBtn").addEventListener("mouseout", function() {
+        document.getElementById("demo").innerHTML = "Thanks for fill this Form if you sign in";
+    this.style.backgroundColor = "purple";
+    
+    });
+//     var name=prompt("Enter Your Name Please: ")
+//     if(name!==''){
+//     alert("Thanks For View My Website "+name);
+// }
